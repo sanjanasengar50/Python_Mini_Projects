f4b4902c3a7a5dc0a594ba7fd921321619fa1caa
Sanjana Singh Sengar <sanjanasengar50@gmail.com>
2026-07-10 12:58:26 +0530

    Add files via upload

---
+++ slideshow.py
@@ -0,0 +1,40 @@
+from itertools import cycle
+from PIL import Image, ImageTk
+import time 
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Image Slideshow Viewer")
+
+#List of image Path
+image_paths =[
+    r"images\girl1.png",
+    r"images\girl2.png"
+    r"images\girl3.png"
+    r"images\girl4.png"
+] 
+
+#Resize the image to 1080x1080
+image_size = (1080,1080)
+images = [Image.open(path).resize(image_size) for path in image_paths]
+photo_images = [ImageTk.PhotoImage(image) for image in images]
+
+label= tk.Label(root)
+label.pack()
+
+def update_image():
+    for photo_image in photo_images:
+        label.config(image = photo_image)
+        label.update()
+        time.sleep(3)
+slideshow = cycle(photo_images)
+def start_slideshow():
+    for _ in range(len(image_paths)):
+        update_image()
+play_button = tk.Button(root, text = 'Play Slideshow', command = start_slideshow)
+play_button.pack()
+
+root.mainloop()
+
+
+
